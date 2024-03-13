d77f3430a427fd5fd2c743943179386220fa7a43
DaBiggestPhrog <161621840+DaBiggestPhrog@users.noreply.github.com>
2024-03-13 16:14:17 +0000

    Generacja liczb - działania

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i <= 100; i += 4) {
-            System.out.println(i);
+        Random los = new Random();
+        int suma = 0;
+        for (int i = 0; i < 5; i++) {
+            int Liczba = los.nextInt(101);
+            int kwadrat = Liczba * Liczba;
+            suma += kwadrat;
+            System.out.println("Wylosowana liczba[" + Liczba + "], [" + Liczba + "]^2 = " + kwadrat);
         }
+        System.out.println("Suma wszystkich kwadratów: " + suma);
     }
 }
