42825cae664131f6869f2b3be307252584c05034
DaBiggestPhrog <161621840+DaBiggestPhrog@users.noreply.github.com>
2024-03-13 15:48:40 +0000

    Hello Word - krzywe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+      String Zaba = "Hello Word";
 
+      for (int i = 0; i < Zaba.length(); i++) {
+        for (int j = 0; j < i; j++) {
+            System.out.print(" ");
+        }
+          System.out.println(Zaba.charAt(i));
+      }
+  }
 }
