6ffa179596eb9f5025b68647a749e6969ec751f5
DaBiggestPhrog <161621840+DaBiggestPhrog@users.noreply.github.com>
2024-03-13 16:02:24 +0000

    Liczby od 0 do 100

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,7 @@
 public class Main {
-  public static void main(String[] args) {
-      String Zaba = "Hello Word";
-
-      for (int i = 0; i < Zaba.length(); i++) {
-        for (int j = 0; j < i; j++) {
-            System.out.print(" ");
+    public static void main(String[] args) {
+        for (int i = 0; i <= 100; i += 4) {
+            System.out.println(i);
         }
-          System.out.println(Zaba.charAt(i));
-      }
-  }
+    }
 }
